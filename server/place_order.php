36076b336fdf2f0fc50ebb94f521001a1108b39b
sarathrajknr <sarathrajknr@gmail.com>
2024-08-07 21:59:34 +0530

    Update place_order.php
    an attempt made

--- server/place_order.php
+++ server/place_order.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 session_start();
 
 include('connection.php');
@@ -14,17 +13,20 @@ if (isset($_POST['place_order'])) {
     $address = $_POST['address'];
     $order_cost = $_SESSION['total'];
     $order_status = "on_hold";
-    $user_id = 1; // Ideally, this should be fetched from session or user authentication
+    $user_id = 1; 
     $order_date = date('Y-m-d H:i:s');
 
     // Prepare SQL statement to insert order
     $stmt = $conn->prepare("INSERT INTO orders (order_cost, order_status, user_id, user_phone, user_city, user_address, order_date) VALUES (?, ?, ?, ?, ?, ?, ?)");
 
+    
+
     // Bind parameters to the SQL query
-    $stmt->bind_param('isiisss', $order_cost, $order_status, $user_id, $user_phone, $user_city, $user_address, $order_date);
+    $stmt->bind_param('isiisss', $order_cost, $order_status, $user_id, $phone, $city, $address, $order_date);
 
     // Execute the statement
     $stmt->execute();
+       
 
     // Retrieve the inserted order ID
     $order_id = $stmt->insert_id;
@@ -32,31 +34,13 @@ if (isset($_POST['place_order'])) {
     // Output the order ID
     echo $order_id;
 
- 
+
+
+    //2. Get products from the cart 
 
     
 
-
-
-    //2. get product from cart
-
-
-    //3. issue new order & store order info in database
-
-
-
-    //4. store each single item in order_items database
-
-    
-
-    //5. remove everything from cart
-
-
-
-    //6. infom user wheather everything is fine or there is a problem
-
-    
+   
 }
-
 
 ?>
